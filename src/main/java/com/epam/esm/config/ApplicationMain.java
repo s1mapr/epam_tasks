f8d02f7d9fd7ef0be5ded6cf99fc2e375b1b818f
s1mapr <113846506+s1mapr@users.noreply.github.com>
2023-09-19 09:29:18 +0300

    Added configure method

--- src/main/java/com/epam/esm/config/ApplicationMain.java
+++ src/main/java/com/epam/esm/config/ApplicationMain.java
@@ -3,12 +3,18 @@ package com.epam.esm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("com.epam.esm.entity")
 @ComponentScan("com.epam.esm")
-public class ApplicationMain {
+public class ApplicationMain extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApplicationMain.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
     }
