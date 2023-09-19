5f3701c3a0845c0b7faa5f2c1fe916b9e2891156
s1mapr <113846506+s1mapr@users.noreply.github.com>
2023-09-19 09:39:11 +0300

    returned to prev version

--- src/main/java/com/epam/esm/config/ApplicationMain.java
+++ src/main/java/com/epam/esm/config/ApplicationMain.java
@@ -3,18 +3,12 @@ package com.epam.esm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("com.epam.esm.entity")
 @ComponentScan("com.epam.esm")
-public class ApplicationMain extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ApplicationMain.class);
-    }
+public class ApplicationMain {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
     }
