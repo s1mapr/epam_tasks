d715ba070f34aaecdd82f49b461e044c38c331a3
s1mapr <113846506+s1mapr@users.noreply.github.com>
2023-09-19 10:11:24 +0300

    added parent class

--- src/main/java/com/epam/esm/config/ApplicationMain.java
+++ src/main/java/com/epam/esm/config/ApplicationMain.java
@@ -3,12 +3,13 @@ package com.epam.esm.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("com.epam.esm.entity")
 @ComponentScan("com.epam.esm")
-public class ApplicationMain {
+public class ApplicationMain extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationMain.class);
     }
